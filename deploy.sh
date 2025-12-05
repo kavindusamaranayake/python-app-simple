bb41d5eedec38a8fa7dd454293174dfc5980017a
kavindusamaranayake <k.samaranayake0026@gmail.com>
2025-12-05 18:13:35 +0530

    linux

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+PROJECT_DIR="/home/kavindu/projects-devops/python-app-simple"
+DOCKER_IMG="python-sample-app"
+IMG_TAG="latest"
+CONTAINER_NAME="python-sample-app"
+
 # Navigate to app directory
-cd /home/kavindu/projects-devops/python-app-simple
+cd $PROJECT_DIR
 
 # Pull latest code from Git
 git pull origin main
 
 # Build Docker image
-docker build -t python-sample-app:latest .
+docker build -t $DOCKER_IMG:$IMG_TAG .
 
 # Stop & remove existing container if exists.
-docker stop python-sample-app || true
-docker rm python-sample-app || true
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
 
 # Run new container
-docker run -d --name python-sample-app -p 5002:5000 python-sample-app:latest
+docker run -d --name $CONTAINER_NAME -p 5002:5000 $DOCKER_IMG:$IMG_TAG
 
 echo "Deployment successful at $(date)"
